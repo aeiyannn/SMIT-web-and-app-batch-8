b528351166ef7728b7b6cde06c1a61d3c9db8b88
aeiyan <aeiyankhan2@gmail.com>
2025-11-01 14:17:05 +0500

    upload code

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello World"); // console.log is a JavaScript method used to display output in the browser console.
+
+alert("Hello, this is a popup and we are learning JavaScript."); // alert is a built-in function used to display a popup message.
+
+var firstName = "Aeiyan"; // A variable is a way to store data in JavaScript.
+var age = 22; // We can also store numbers in a variable.
+
+console.log(firstName, age); // We can display variables in the console using console.log.
+alert(firstName + age); // We can concatenate (join) two variables using the + operator in JavaScript.
