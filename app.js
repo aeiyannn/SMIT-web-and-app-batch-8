a03bbcba8f6f721da5d4dfa77b9d91835cf63560
aeiyan <110762239+aeiyannn@users.noreply.github.com>
2025-11-09 16:03:07 +0500

    upload code

---
+++ app.js
@@ -0,0 +1,54 @@
+// var num=prompt("Enter any number to generate a Table")
+// console.log(num)
+// alert(
+//     num + " X 1 = " + num + "\n" +
+//     num + " X 2 = " + num * 2 + "\n" +
+//     num + " X 3 = " + num * 3 + "\n" +
+//     num + " X 4 = " + num * 4 + "\n" +
+//     num + " X 5 = " + num * 5 + "\n" +
+//     num + " X 6 = " + num * 6 + "\n" +
+//     num + " X 7 = " + num *7 + "\n" +
+//     num + " X 8 = " + num * 8 + "\n" +
+//     num + " X 9 = " + num * 9 + "\n" +
+//     num + " X 10 = " + num * 10
+
+// )
+
+// var age = parseInt(prompt("Enter Your age"))
+// // console.log(age === 18)
+// if (age >= 18) {
+//     console.log("You are 18+ you can enter")
+// }
+// else {
+//   console.log("you can,t access this website")  
+
+// }
+// var stringNum = parseInt("16yyy52")
+// console.log(stringNum)
+
+// A1 = 80-100
+// A = 70 -79
+// B= 60 - 69
+// C =50 -59
+//Fail less than 50 
+ var percentage=+prompt("Enter your percentange")
+ if(percentage >=80) {
+    alert ("Congrats Your grade is A1")
+ }
+ else if(percentage >= 70){
+    alert("A")
+ }
+ else if (percentage >=60){
+    alert("B")
+ }
+ else if(percentage >=50) {
+    alert("C")
+ }
+ else {
+    alert("you are failed")
+ }
+
+
+
+
+
