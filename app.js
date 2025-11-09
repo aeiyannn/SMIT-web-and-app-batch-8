0066264c683a38bfafa7f513a94bd4f9e5610e4c
aeiyan <110762239+aeiyannn@users.noreply.github.com>
2025-11-09 16:02:14 +0500

    upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// var counter=1
+// console.log(counter)
+// counter=2
+// console.log(counter)
+// counter=counter + 1 // 2 + 1
+// console.log(counter) //3
+// counter=counter + 1 // 3 +1
+// console.log(counter) //4
+// counter=counter + 1  // 4 + 1
+// console.log(counter)
+
+// var counter = 10
+// console.log(counter,13) // 10
+// console.log(--counter,14) // 9
+// console.log(--counter,15) // 8
+// console.log(--counter,16) // 7
+// console.log(++counter) // 7 + 1
+// console.log(counter)
+
+// var counter = 1;
+// console.log(counter++) // 
+// console.log(++counter)  //
+
+// var message1 = "hello \n \n"
+// var message2 = "How are you"
+// var fullmessage= message1 + "6" + message2 + 7
+// alert(fullmessage)
+var age= +prompt("what is your age?","Enter your age")  
+
+if(age==18) {
+    alert("The Enter age is 18")
+}  
+
+
+
