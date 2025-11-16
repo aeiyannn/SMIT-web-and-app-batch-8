ad13df3a6a74b67255400281d2c8320a3e2efbbe
aeiyan <aeiyankhan2@gmail.com>
2025-11-16 14:27:32 +0500

    upload code

---
+++ app.js
@@ -0,0 +1,8 @@
+// for loop
+
+//i=0  // 1 se start karo
+// ye i jab tak equal ya chota rhy 10 se jab tak chalo
+//i++ aik number increase // i+1
+for (var i = 1; i <= 10; i++) {
+  console.log("7 X " + i + "=" + 7 * i);
+}
