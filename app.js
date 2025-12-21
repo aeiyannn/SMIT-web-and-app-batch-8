28cedf150c6d70365e56bbbe7d8696ea9d5c44c0
aeiyan <aeiyankhan2@gmail.com>
2025-12-21 17:29:34 +0500

    upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var fruits=['peach', 'mango', 'apple', 'graphes', 'bananna', 'guvava', 'orange',"new","new"]
+var number=[1,2,4,5,7,8,9,10,11]
+// fruits.unshift("Mango")
+// fruits.unshift("peach")
+// fruits.shift()
+// fruits.splice(1,0,"Stawberry")
+// fruits.splice(2,fruits.length - 2)
+number.sort((a,b)=>a-b)
+console.log(number)
+// for(var i=0;i<5;i++){
+//     console.log(fruits[i])
+// }
+// for (var i in fruits) {
+//     console.log(fruits[i])
+// }
