ea7b77d3fb1071f83c0fdc5b0c5d709ef5277415
aeiyan <aeiyankhan2@gmail.com>
2025-12-21 17:35:32 +0500

    upload code

---
+++ app.js
@@ -0,0 +1,9 @@
+// const introDOMById= document.getElementById("introDOM")
+// console.log(introDOMById)
+// const introDOM = document.getElementsByClassName("xyz")
+// console.log(introDOM[1])
+// const intro=document.getElementsByTagName("h1")
+// console.log(intro[1])
+
+const intro =document.querySelector(".h1Style")
+intro.style.color="green"
