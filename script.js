08e1c5fd9566ee04d87acd08640c00329a7563bf
aeiyan <aeiyankhan2@gmail.com>
2025-12-21 17:31:11 +0500

    upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var studentsA=["Ahmed","Sami","Ali","Moiz","Bhatti","Ali"]
+var studentsB=["Usama","Shahzaib","Babar","Moiz","Bhatti","Ali"]
+
+// var mergeStudentData=studentsA.concat(studentsB)
+// console.log(mergeStudentData)
+var numbers=[1,3,7,6,4,2,-1,5,10,8,9,0,]
+console.log(numbers.sort((a,b)=>b-a))
+
+var firstName="Aeiyan khan"
+console.log(firstName.charCodeAt("A"))
+
+var students="    Hello   World    "
+console.log(students)
+var  str = ["hello","world"];
+console.log(str.includes("world"))
