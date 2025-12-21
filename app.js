4ec453f15b2b10e4fcae8950aca3b3ad96dec51a
aeiyan <aeiyankhan2@gmail.com>
2025-12-21 17:30:01 +0500

    upload codeupload

---
+++ app.js
@@ -0,0 +1,26 @@
+// // var name1="Tayyab"
+// // console.log(name1)
+// // name1=name1 + "Khan"
+// // name1+="Khan"
+// // console.log(name1)
+// // name1=name1+"Khan"
+// var number=2
+// console.log(number)
+// // number=number *3
+// number*=3
+// console.log(number)
+var studentData=["Ahmed","Tayyab","Aeiyan","Hassnain","Moiz","Wali","Raheem","Hamza","imran Khan","Qasim"]
+// studentData[10]="Ali"
+studentData.push("Ahmed")
+studentData.push("ali","khan","imam")
+console.log(studentData)
+// console.log(studentData)
+// studentData[4]="Sami"
+// console.log(studentData[0],studentData[1])
+// for(var i=0;i<=9;i++){
+//     console.log(i,studentData[i])
+
+// }
+
+
+
