8748af6c72d4c89d516cfc1e9828022154013c80
aeiyan <aeiyankhan2@gmail.com>
2025-12-21 17:33:19 +0500

    upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// VAR VS LET VS CONST
+
+// var studentName = "Aeiyan Khan";
+// var studentName = "Ahmed"
+// console.log(studentName)
+
+// LET
+// let studentName = "Ahmed";
+// let studentName = "Ali"
+// console.log(studentName)
+
+// COnst 
+
+// const studentName = "Ahmed"
+
+// console.log(studentName)
+
+
+// Simple Functions
+
+// function intro() {
+//     console.log("Hello this is Aeiyan")
+// }
+
+// intro()
+// intro()
+// intro()
+
+// func with parameter 
+// function intro(name, age, course) {
+//     console.log("Welcome " + " " + name + age + course)
+// }
+// intro("Waleed", 20, 'WEB and App')
+addition(4, 5)
+function addition(num1, num2) {
+    console.log(+num1 + +num2)
+}
+
+// intro("Aeiyan")
