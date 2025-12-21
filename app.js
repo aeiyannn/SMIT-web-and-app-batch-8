ef22d764d8bbe933f2df4326f7350d0b2a8ed0b9
aeiyan <aeiyankhan2@gmail.com>
2025-12-21 17:34:53 +0500

    uupload code

---
+++ app.js
@@ -0,0 +1,16 @@
+// Selectors
+// by Id
+// const intro = document.getElementById("intro")
+// intro.innerHTML = "<mark> This is bold text </mark>"
+// By ClassName
+// const introClass = document.getElementsByClassName("intro-1")
+// introClass[0].textContent = "DOm"
+// By Tag Name
+// const introTag = document.getElementsByTagName("h1")
+// introTag[0].textContent = "This text come from DOM"
+// const querySelector = document.querySelector("h1")
+// querySelector.innerHTML = "<i>THis is a query Selector</i> <br> <b>THis is a query Selector</b>"
+
+
+// const querySelectorAll = document.querySelectorAll(".intro")
+// querySelectorAll[0].textContent = "1"
