4a4f562a7feffe26b723c33dff509252c0efe472
aeiyan <110762239+aeiyannn@users.noreply.github.com>
2026-02-19 23:01:48 +0500

    upload code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+function App(){
+  return (
+    <>
+    <h1 style={{backgroundColor:"green",color:"white"}}>Hello React</h1>
+    <b className="bold">First Class Of React</b>
+    </>
+  )
+}
+
+export default App
